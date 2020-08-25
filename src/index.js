596068369f5878c97f9efcb87266361723ebcd4e
chan <thechanmoon@gmail.com>
2020-08-25 10:29:30 -0400

    error fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-test
+const add = document.getElementById('add')
+const minus = document.getElementById('minus')
+const number = document.querySelector('span')
+
+let count = 0
+
+number.innerText = count
+
+const updateText = () => {
+	number.innerText = count
+}
+
+const handleAdd = () => {
+	// console.log('handleadd')
+	count++
+	updateText()
+}
+const handleMinus = () => {
+	count--
+	updateText()
+}
+
+add.addEventListener('click', handleAdd)
+minus.addEventListener('click', handleMinus)
