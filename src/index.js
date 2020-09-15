1265100e598fc8b243e66f6d04be7ab9f18ae917
chanmoonpark <thechanmoon@gmail.com>
2020-09-15 00:04:17 -0500

    Todo List Form & redux added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,55 @@
-const add = document.getElementById('add')
-const minus = document.getElementById('minus')
-const number = document.querySelector('span')
+import { createStore } from 'redux'
 
-let count = 0
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const ul = document.querySelector('ul')
 
-number.innerText = count
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
 
-const updateText = () => {
-	number.innerText = count
+const reducer = (state = [], action) => {
+	console.log(action)
+	// return state
+	switch (action.type) {
+		case ADD_TODO:
+			return [action.text, ...state]
+		case REMOVE_TODO:
+			return []
+		default:
+			return state
+	}
 }
+const store = createStore(reducer)
+form.addEventListener('submit', (e) => {
+	e.preventDefault()
+	let li = document.createElement('li')
+	let value = input.value
+	li.innerText = value
+	input.value = ''
+	ul.appendChild(li)
+	store.dispatch({ type: ADD_TODO, text: value })
+})
+// const add = document.getElementById('add')
+// const minus = document.getElementById('minus')
+// const number = document.querySelector('span')
 
-const handleAdd = () => {
-	// console.log('handleadd')
-	count++
-	updateText()
-}
-const handleMinus = () => {
-	count--
-	updateText()
-}
+// let count = 0
 
-add.addEventListener('click', handleAdd)
-minus.addEventListener('click', handleMinus)
+// number.innerText = count
+
+// const updateText = () => {
+// 	number.innerText = count
+// }
+
+// const handleAdd = () => {
+// 	// console.log('handleadd')
+// 	count++
+// 	updateText()
+// }
+// const handleMinus = () => {
+// 	count--
+// 	updateText()
+// }
+
+// add.addEventListener('click', handleAdd)
+// minus.addEventListener('click', handleMinus)
