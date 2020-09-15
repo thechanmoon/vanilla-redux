a0a4d88fac86814c4d6f03c1f63271973c42ee7c
chan <thechanmoon@gmail.com>
2020-09-15 19:11:37 +0000

    remove added

--- src/index.js
+++ src/index.js
@@ -12,23 +12,58 @@ const reducer = (state = [], action) => {
 	// return state
 	switch (action.type) {
 		case ADD_TODO:
-			return [action.text, ...state]
+			return [{ text: action.text }, ...state]
 		case REMOVE_TODO:
 			return []
 		default:
 			return state
 	}
 }
+
+const renderDodoList = () => {
+	const text = store.getState()
+	ul.innerText = ''
+	text.forEach((element, index) => {
+		console.log(element)
+		let li = document.createElement('li')
+		let button = document.createElement('button')
+		li.innerText = element.text
+		li.id = 'li-' + index
+		console.log('li : ' + li)
+		console.log('li : ' + li.id)
+		button.innerText = 'Del'
+		button.id = index
+		button.addEventListener('click', removeButtonHandler)
+		ul.appendChild(li)
+		ul.appendChild(button)
+	})
+}
+
 const store = createStore(reducer)
-form.addEventListener('submit', (e) => {
+store.subscribe(renderDodoList)
+const addText = (text) => {
+	store.dispatch({ type: ADD_TODO, text: text })
+}
+
+const addButtonHandler = (e) => {
 	e.preventDefault()
-	let li = document.createElement('li')
-	let value = input.value
-	li.innerText = value
+	let text = input.value
 	input.value = ''
-	ul.appendChild(li)
-	store.dispatch({ type: ADD_TODO, text: value })
-})
+	addText(text)
+}
+
+const removeButtonHandler = (e) => {
+	e.preventDefault()
+	console.log('removeButtonHandler', e, e.target, e.target.id)
+	// console.log('removeButtonHandler', )
+	const li = document.getElementById('li-' + e.target.id)
+	console.log(li)
+	e.target.remove()
+	li.remove()
+}
+
+form.addEventListener('submit', addButtonHandler)
+console.log(store)
 // const add = document.getElementById('add')
 // const minus = document.getElementById('minus')
 // const number = document.querySelector('span')
